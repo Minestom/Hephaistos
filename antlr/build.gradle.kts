84ef529e004ed439ad3709b0032453a6b9c6a9a6
jglrxavpok <jglrxavpok@gmail.com>
2021-08-21 13:36:34 +0200

    v2.0.1 (forgot to publish antlr jar)

--- antlr/build.gradle.kts
+++ antlr/build.gradle.kts
@@ -1,4 +1,17 @@
+plugins {
+    `maven-publish`
+    id("convention.publication")
+}
+
 dependencies {
     // https://mvnrepository.com/artifact/org.antlr/antlr4-runtime
     implementation("org.antlr:antlr4-runtime:4.8-1")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ allprojects {
     apply(plugin = "org.jetbrains.dokka")
 
     group = "io.github.jglrxavpok.hephaistos"
-    version = "2.0.0"
+    version = "2.0.1"
 
     repositories {
         mavenCentral()
