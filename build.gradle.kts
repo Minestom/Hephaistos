f49784fba37e245b3c17c169bd8c14cdf1ba024b
jglrxavpok <jglrxavpok@gmail.com>
2021-12-10 21:42:21 +0100

    2.3.0 release

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ allprojects {
     apply(plugin = "org.jetbrains.dokka")
 
     group = "io.github.jglrxavpok.hephaistos"
-    version = "2.3.0-rc3"
+    version = "2.3.0"
 
     repositories {
         mavenCentral()
