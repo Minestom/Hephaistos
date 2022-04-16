c6f11ab73989720a06be32c9efb98759de117cc0
jglrxavpok <jglrxavpok@gmail.com>
2022-04-16 23:40:10 +0200

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ allprojects {
     apply(plugin = "org.jetbrains.dokka")
 
     group = "io.github.jglrxavpok.hephaistos"
-    version = "2.4.2"
+    version = "2.4.3"
 
     repositories {
         mavenCentral()
