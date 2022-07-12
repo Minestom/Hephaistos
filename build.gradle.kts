cec720e1a5562186a091a64ce2b9a256e0328674
jglrxavpok <jglrxavpok@gmail.com>
2022-07-12 22:28:28 +0200

    Increment version to 2.4.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ allprojects {
     apply(plugin = "org.jetbrains.dokka")
 
     group = "io.github.jglrxavpok.hephaistos"
-    version = "2.4.7"
+    version = "2.4.8"
 
     repositories {
         mavenCentral()
