f413e87338f250b58f3d38357cc49a603431c14f
jglrxavpok <jglrxavpok@gmail.com>
2022-10-02 10:38:13 +0200

    v2.5.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ allprojects {
     apply(plugin = "org.jetbrains.dokka")
 
     group = "io.github.jglrxavpok.hephaistos"
-    version = "2.5.1"
+    version = "2.5.2"
 
     repositories {
         mavenCentral()
